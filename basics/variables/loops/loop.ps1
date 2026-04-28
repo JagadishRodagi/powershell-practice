4164208feb1208ca2a5955402ff84bf2faee80d7
Jagadish Rodagi <115778746+JagadishRodagi@users.noreply.github.com>
2026-04-28 15:10:46 +0530

    Create loop.ps1

---
+++ basics/variables/loops/loop.ps1
@@ -0,0 +1,11 @@
+for ($i = 1; $i -le 5; $i++) {
+  Write-Host "Number: $i"
+  }
+
+  # $i = 1 = Start value
+  # $i -le 5 -> Loop condition
+  # $i++ -> Increase by 1
+  # Loop runs 5 times
+  #Used for automation & bulk tasks
+
+  
