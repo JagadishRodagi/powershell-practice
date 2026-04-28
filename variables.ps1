0d6a2b6e0e8412fd0e91d69d2510b738c3d7822c
Jagadish Rodagi <115778746+JagadishRodagi@users.noreply.github.com>
2026-04-28 15:06:56 +0530

    Update variables.ps1

--- variables.ps1
+++ variables.ps1
@@ -1,1 +1,14 @@
+#Variables and Output
+
+$name = "Jagadish"
+$role = "Powershell Learner"
+
+Write-Host "Name: $name"
+Write-Host "Role: $role"
+
+Explaination:
+#$name, $role -> Varibles to store values
+# $variables inside quotes -> Value is replaced automatically
+Write-Host -> Prints variables values
+
 
